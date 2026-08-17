df1051dc1d596604d01c9c5502d19e19b390e426
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2026-08-17 21:31:14 +0100

    Test

---
+++ test_connection.py
@@ -0,0 +1,23 @@
+"""
+test_connection.py
+Just checks that Python can talk to your bank_transaction database.
+Run: python test_connection.py
+"""
+
+import psycopg2
+
+conn = psycopg2.connect(
+    dbname="bank_transactions",
+    user="postgres",
+    password="Hezekiah1!",
+    host="localhost",
+    port="5432",
+)
+
+cur = conn.cursor()
+cur.execute("SELECT COUNT(*) FROM transactions;")
+count = cur.fetchone()[0]  # This allows me to grabs exactly one row from your database query results.
+print(f"Connected successfully! Row count in transactions table: {count}")
+
+cur.close() # close the execution to free up resources
+conn.close() # close the connection 
