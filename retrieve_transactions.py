03086f484332720059f3621990b45a8c3dd41c7d
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2026-08-17 21:32:58 +0100

    Task 5: Database operations: Retrieve

--- retrieve_transactions.py
+++ retrieve_transactions.py
@@ -15,7 +15,7 @@ import psycopg2
 conn = psycopg2.connect(
     dbname="bank_transactions",
     user="postgres",
-    password="Hezekiah1!",
+    password="YOUR_PASSWORD",
     host="localhost",
     port="5432",
 )
